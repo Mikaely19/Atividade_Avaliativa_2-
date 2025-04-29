9356cc4b8c97406a0ea720011b8c56159c5cf835
Mikaely19 <125111409306@ulife.com.br>
2025-04-28 23:17:19 -0300

    Create Fivonacci.java

---
+++ Fivonacci.java
@@ -0,0 +1,21 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Sequencia Fibonacci");
+
+        int var1 = 0;
+        int var2= 1;
+        Int proximo; 
+        int soma;
+
+        System.out.println(var1); // Imprimir o primeiro valor
+
+        while(numero < 9) { // Imprimir os 9 restantes
+            System.out.println(var2);
+            proximo = var1 + var2;
+            var1 = var2;
+            var2 = proximo;
+            soma++;
+
+        }
+    } 
+}
