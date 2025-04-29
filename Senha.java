5b9625059c9c7ac9599b38b269d3681b411285ae
Mikaely19 <125111409306@ulife.com.br>
2025-04-28 23:16:02 -0300

    Create Senha.java

---
+++ Senha.java
@@ -0,0 +1,22 @@
+ublic class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Senha");
+
+        Scanner scanner + new Scanner(System.in);
+        String senhacorreta = "1234"
+
+        while(true){
+            System.out.print("Digite a senha:");
+        
+            if(senhacorreta){
+                Sytem.out.println("Senha correta");
+                break;
+            } else {
+                System.out.println("Senha incorreta, indique novamente:");
+            }
+        }
+
+        scanner.close();
+
+    }
+}
