dfb47ee507e4f8d61ff79a73313fd60cf7845090
Mikaely19 <125111409306@ulife.com.br>
2025-04-28 23:18:36 -0300

    Create EntradadeDados.java

---
+++ EntradadeDados.java
@@ -0,0 +1,22 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Entrada de Dados");
+
+        Scanner scanner = new Scanner (System.in);
+        int numero = 0;
+
+        do {
+            System.out.print("Insira um número entre 1 e 100 : ");
+            numero = scanner.next,int();
+            System.out.println("i = " + i);
+            } while ( i>=0 ; numero < 1 numero >100)
+
+            if(numero < 1; numero > 100){
+                System.out.println("Número incorreto")
+
+                System.out.println("Número válido: " + numero);
+                scanner.close();
+            }
+        }
+
+    }
