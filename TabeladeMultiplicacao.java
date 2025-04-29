bdef8ab7b9f4bc077101e9f1d8830aab93619b1a
Mikaely19 <125111409306@ulife.com.br>
2025-04-28 23:26:06 -0300

    Create TabeladeMultiplicacao.java

---
+++ TabeladeMultiplicacao.java
@@ -0,0 +1,13 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Tabela de Multiplicação");
+        
+        Int numero = 5 ;
+        for ( int i = 1; i <= 10; i ++){
+            int resultado = numero * i;
+            System.out.println("5  * " + i + " = " + resultado);
+
+        }
+
+    }
+}
