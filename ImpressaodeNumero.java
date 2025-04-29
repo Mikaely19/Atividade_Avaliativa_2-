0314a7e8ebf38fa9fd0e90ca5115324991a42975
Mikaely19 <125111409306@ulife.com.br>
2025-04-28 23:33:25 -0300

    Create ImpressaodeNumero.java

---
+++ ImpressaodeNumero.java
@@ -0,0 +1,16 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Impressão do número:");
+
+        Valor = 50;
+        Scanner scanner = new Scanner (System.in);
+        while (valor != 0) {
+        System.out.println("O valor atual é: " + valor);
+        valor--;
+        }
+
+        System.out.println(x: "Contagem finalizada.")
+        scanner.close();
+    
+    }
+}
